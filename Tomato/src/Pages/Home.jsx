c88e86814acd9922f4162dc88ce95983fd368a6b
wtfxdl <fadil.sr.jalal@gmail.com>
2025-08-21 17:47:54 +0530

    commit

--- Tomato/src/Pages/Home.jsx
+++ Tomato/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Components/Header'
+import Menu from '../Components/Menu'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <Menu/>
       
     </div>
   )
