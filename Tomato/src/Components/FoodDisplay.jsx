eefb77756b2e894e9ede88331a341cfedd307672
wtfxdl <fadil.sr.jalal@gmail.com>
2025-08-22 12:27:54 +0530

    commit

--- Tomato/src/Components/FoodDisplay.jsx
+++ Tomato/src/Components/FoodDisplay.jsx
@@ -16,6 +16,6 @@ const FoodDisplay = ({ menulist }) => {
             </div>
         </div>
     )
-}
+}   
 
 export default FoodDisplay
--- Tomato/src/Components/FoodItem.jsx
+++ Tomato/src/Components/FoodItem.jsx
@@ -1,24 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/frontend_assets/assets'
 
-const FoodItem = ({id,name,price,description,image}) => {
+const FoodItem = ({ id, name, price, description, image }) => {
+    const [itemcount,setItemCound] = useState(0);
   return (
     <div className='food-item'>
-        <div className="food-item image-container">
-            <img className='food-item-image' src={image} alt="" />
-            <div className="food-item-info">
-                <div className='food-item-rating'>
-                    <p>{name}</p>
-                    <img src={assets.rating_starts} alt="" />
-                    <div>
-                        <p className='food-item-des'>{description}</p>
-                        <p className="food-item-price">${price}</p>
-                    </div>
-
-
-                </div>
-            </div>
+      <img className='food-item-image' src={image} alt={name} />
+      {!itemcount
+      }
+      <div className='food-item-info'>
+        <div className='food-item-rating'>
+          <p className='food-item-name'>{name}</p>
+          <img className='food-item-stars' src={assets.rating_starts} alt="rating" />
         </div>
+        <p className='food-item-desc'>{description}</p>
+        <p className='food-item-price'>${price}</p>
+      </div>
     </div>
   )
 }
