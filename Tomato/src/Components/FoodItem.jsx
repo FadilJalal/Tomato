6e4bd851f734e06a30e4345d7826d843a274da58
wtfxdl <fadil.sr.jalal@gmail.com>
2025-09-11 10:00:55 +0530

    commit

--- Tomato/src/Components/FoodItem.jsx
+++ Tomato/src/Components/FoodItem.jsx
@@ -1,31 +1,31 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/frontend_assets/assets';
 
-const FoodItem = ({id,name,price,description,image}) => {
+const FoodItem = ({ id, name, price, description, image }) => {
 
-  const [itemCount,setItemCount] = useState(0);
+  const [itemCount, setItemCount] = useState(0);
 
   return (
     <div className='food-item'>
       <div className="food-item-image-container">
-        <img className='food-item-image' src={image} alt={name}/>
-        { !itemCount ?
-          <img className='add' onClick={()=> setItemCount(itemCount + 1)} src={assets.add_icon_white} alt='add'/>
-          : <div className='food-item-counter'>
-            <img onClick={()=>setItemCount(itemCount - 1)} src={assets.remove_icon_red} alt='remove'/>
-            <p>{itemCount}</p>
-            <img onClick={()=>setItemCount(itemCount + 1)} src={assets.add_icon_green} alt='add'/>
-          </div>
+        <img className='food-item-image' src={image} alt={name} />
+        {
+          !itemCount
+            ? <img className='add' src={assets.add_icon_white} onClick={() => setItemCount(itemCount + 1)} />
+            : <div className='food-item-counter'>
+              <img src={assets.remove_icon_red} onClick={() => setItemCount(itemCount - 1)} />
+              <p>{itemCount}</p>
+              <img src={assets.add_icon_green} onClick={() => setItemCount(itemCount + 1)} />
+            </div>
         }
       </div>
       <div className='food-item-info'>
         <div className='food-item-name-rating'>
           <p>{name}</p>
-          <img src={assets.rating_starts}/>
+          <img src={assets.rating_starts} />
         </div>
         <p className="food-item-desc">{description}</p>
         <p className="food-item-price">$ {price}</p>
-
       </div>
     </div>
   )
