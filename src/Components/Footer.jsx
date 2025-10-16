6e7078391043798155c0ad75c95aeaa3a0408d39
Fadil Jalal <Fadil.sr.jalal@gmail.com>
2025-10-16 13:51:50 +0400

    initial commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,21 +3,29 @@ import { assets } from '../assets/frontend_assets/assets';
 
 const Footer = () => {
   return (
-    <div className='footer' id='footer'>
+    <footer className="footer" id="footer">
       <div className="footer-content">
         {/* Left Section */}
-        <div className="footer-content-left">
-          <img src={assets.logo} alt="Tomato Logo" />
-          <p>Delivering delicious meals from your favorite restaurants straight to your doorstep, fast and fresh every time.</p>
+        <div className="footer-section footer-left">
+          <img src={assets.logo} alt="Tomato Logo" className="footer-logo" />
+          <p>
+            Delivering delicious meals from your favorite restaurants straight to your doorstep, fast and fresh every time.
+          </p>
           <div className="footer-social-icons">
-            <img src={assets.facebook_icon} alt="Facebook" />
-            <img src={assets.twitter_icon} alt="Twitter" />
-            <img src={assets.linkedin_icon} alt="LinkedIn" />
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <img src={assets.facebook_icon} alt="Facebook" />
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <img src={assets.twitter_icon} alt="Twitter" />
+            </a>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              <img src={assets.linkedin_icon} alt="LinkedIn" />
+            </a>
           </div>
         </div>
 
         {/* Center Section */}
-        <div className="footer-content-center">
+        <div className="footer-section footer-center">
           <h2>COMPANY</h2>
           <ul>
             <li>Home</li>
@@ -28,7 +36,7 @@ const Footer = () => {
         </div>
 
         {/* Right Section */}
-        <div className="footer-content-right">
+        <div className="footer-section footer-right">
           <h2>GET IN TOUCH</h2>
           <ul>
             <li>+971 0555696002</li>
@@ -37,12 +45,12 @@ const Footer = () => {
         </div>
       </div>
 
-      <hr />
-      <p className="footer-copyright">
-        Copyright 2024 Ⓒ Tomato.com - All Rights Reserved
+      <hr className="footer-divider" />
+      <p className="footer-bottom-text">
+        &copy; 2024 Tomato.com - All Rights Reserved
       </p>
-    </div>
+    </footer>
   );
-}
+};
 
 export default Footer;
